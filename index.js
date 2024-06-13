7025cdea2ef386b19a5766c56a954251c4ac509a
Nico Soengas <nicosoengas@hotmail.com>
2024-06-13 11:57:28 +0200

    Fix messages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var messages = ["Happy birthday to GFG",
-	"Happy birthday to GeeksforGeeks",
-	"Happy birthday to Geeks"];
+var messages = ["Feliz cumple Cin!!!",
+	"Buen dia para todos alla en comarca El Maresme",
+	"Nos vemos esta noche"];
 
 var i = messages.length;
 var s = Math.floor(Math.random() * i);
